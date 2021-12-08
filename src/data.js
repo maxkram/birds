326854989f7c4f214e3d06c64c02c13a26a5ac7f
maxkram <max.kramer@live.com>
2021-12-07 19:21:51 -0500

    first one

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Wagtail',
+    age: 4,
+    image:
+      'https://images.pexels.com/photos/416179/pexels-photo-416179.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+  },
+  {
+    id: 2,
+    name: 'Parrot',
+    age: 5,
+    image:
+      'https://images.pexels.com/photos/459198/pexels-photo-459198.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+  },
+  {
+    id: 3,
+    name: 'Sparrow',
+    age: 12,
+    image:
+      'https://images.pexels.com/photos/70069/pexels-photo-70069.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+  },
+  {
+    id: 4,
+    name: 'Bullfinch',
+    age: 5,
+    image:
+      'https://images.pexels.com/photos/1808011/pexels-photo-1808011.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+  },
+  {
+    id: 5,
+    name: 'Owl',
+    age: 7,
+    image:
+      'https://images.pexels.com/photos/2115984/pexels-photo-2115984.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+  },
+];
